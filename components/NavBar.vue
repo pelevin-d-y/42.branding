bf5c37922d1e5ed76351d6885d9102b385d3d9f1
pelevin-d-y <natpuot1992@gmail.com>
2019-06-30 18:59:52 +0300

    added nex pref buttons to project page

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div.nav-bar
-    nuxt-link(:to="'/project/' + (prevProject() + 1)")
+    nuxt-link(class="prev" :to="'/project/' + projects[prevProject()].id")
       | {{ projects[prevProject()].title }}
-    nuxt-link(:to="'/project/' + (nextProject() + 1)")
+    nuxt-link(class="next" :to="'/project/' + projects[nextProject()].id")
       | {{ projects[nextProject()].title }}
 </template>
 
@@ -32,9 +32,46 @@ export default {
     }
   }
 }
-
 </script>
 
-<style>
+<style scoped lang="scss">
+  .nav-bar {
+    display: flex;
+    flex-flow: row nowrap;
+    width: 100%;
+  }
 
+  .prev,
+  .next {
+    width: 50%;
+    background: #3431DC;
+    padding: 56px 12px;
+
+    font-size: 40px;
+    line-height: 56px;
+    letter-spacing: 0.01em;
+    color: #FFFFFF;
+    text-decoration: none;
+    text-align: center;
+    font-weight: 600;
+
+    &:hover {
+      background: linear-gradient(180deg, #3431DC 0%, #615EEE 100%);
+      -webkit-text-stroke: 2px white;
+      color: transparent;
+
+    }
+  }
+
+  @media(max-width: 768px) {
+    .prev,
+    .next {
+      display: flex;
+      flex-flow: row nowrap;
+      align-items: center;
+      justify-content: center;
+      padding: 26px 12px;
+      font-size: 36px;
+    }
+  }
 </style>
