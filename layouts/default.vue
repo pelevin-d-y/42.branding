af1e121b382d8635f446df39dc4bfc6ecaae7f2b
Evgeny Taktarov <je.mouth@gmail.com>
2019-07-30 18:41:16 +0300

    fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,6 @@
 <template>
 
   <div>
-    <form name="contact-us"  hidden>
-      <input type="hidden" name="form-name" value="contact" />
-      <input type="text" name="name" />
-      <input type="text" name="email" />
-      <textarea name="message"></textarea>
-    </form>
     <nuxt />
   </div>
 </template>
