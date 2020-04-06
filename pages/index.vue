d6cf10dc6955b2ee8c4cbade8b66a0ecaec88530
pelevin-d-y <natpuot1992@gmail.com>
2020-04-06 17:35:23 +0300

    fix: matter images, main page text

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
           .logo-shadow(ref="logoShadow")
             LogoShadow
         .logo-text
-          | We create the face for the internet
+          | We create the face of the interntet
       nav.nav
         ul
           li.nav-item
